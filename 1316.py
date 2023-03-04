4a12c0fd165d5553351afbfc2551ab5fcbc3a990
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-03-04 17:16:34 +0900

    [1316]

---
+++ 1316.py
@@ -0,0 +1,41 @@
+'''
+3
+happy
+new
+year
+
+4
+aba
+abab
+abcabc
+a
+
+5
+ab
+aa
+aca
+ba
+bb
+'''
+n = int(input())
+answer = 0
+
+for _ in range(n):
+    word = input()
+    w_list = list(word)
+    word_bank = [] 
+
+    for w in word: # a c a
+        if w not in word_bank: # 없으면
+            word_bank.append(w)
+            continue
+        else: #있으면
+            if not word_bank[-1] == w:
+                answer += 1
+                break
+
+print(n - answer)
+
+        
+            
+        
